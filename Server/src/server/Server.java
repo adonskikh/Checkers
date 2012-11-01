305dc92cc79ad6a6cea4d8712abb57fb13a9de9b
A.O.Donskih <totzhe@yandex.ru>
2012-11-01 22:16:12 +0400

    Обработка клиента у сервера вынесена в отдельный метод.

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -13,33 +13,12 @@ import java.net.*;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to Server side");
+    public static void work(Socket client) throws IOException  {
         BufferedReader in = null;
         PrintWriter out = null;
-
-        ServerSocket servers = null;
-        Socket fromclient = null;
-
-        // create server socket
-        try {
-            servers = new ServerSocket(4444);
-        } catch (IOException e) {
-            System.out.println("Couldn't listen to port 4444");
-            System.exit(-1);
-        }
-
-        try {
-            System.out.print("Waiting for a client...");
-            fromclient = servers.accept();
-            System.out.println("Client connected");
-        } catch (IOException e) {
-            System.out.println("Can't accept");
-            System.exit(-1);
-        }
-
-        in = new BufferedReader(new InputStreamReader(fromclient.getInputStream()));
-        out = new PrintWriter(fromclient.getOutputStream(), true);
+        
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(), true);
         String input, output;
 
         System.out.println("Wait for messages");
@@ -52,7 +31,32 @@ public class Server {
         }
         out.close();
         in.close();
-        fromclient.close();
+        client.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to Server side");
+
+        ServerSocket servers = null;
+        Socket client = null;
+
+        // create server socket
+        try {
+            servers = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.out.println("Couldn't listen to port 4444");
+            System.exit(-1);
+        }
+
+        try {
+            System.out.print("Waiting for a client...");
+            client = servers.accept();
+            System.out.println("Client connected");
+        } catch (IOException e) {
+            System.out.println("Can't accept");
+            System.exit(-1);
+        }
+        Server.work(client);
         servers.close();
     }
 }
