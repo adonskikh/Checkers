5800663fc082c15ca47daf51d4040acaf60ea189
A.O.Donskih <totzhe@yandex.ru>
2012-11-01 20:23:44 +0400

    Simple client and server example.

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -8,12 +8,51 @@ package client;
  *
  * @author totzhe
  */
+import java.io.*;
+import java.net.*;
+
 public class Client {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Welcome to Client side");
+
+        Socket fromserver = null;
+
+        String hostname;
+        if (args.length == 0) {
+            //System.out.println("use: client hostname");
+            //System.exit(-1);
+            hostname = "localhost";
+        } else {
+            hostname = args[0];
+        }
+
+
+        System.out.println("Connecting to... " + hostname);
+
+        fromserver = new Socket(hostname, 4444);
+        BufferedReader in = new BufferedReader(new InputStreamReader(fromserver.getInputStream()));
+        PrintWriter out = new PrintWriter(fromserver.getOutputStream(), true);
+        BufferedReader inu = new BufferedReader(new InputStreamReader(System.in));
+
+        String fuser, fserver;
+
+        while ((fuser = inu.readLine()) != null) {
+            out.println(fuser);
+            fserver = in.readLine();
+            System.out.println(fserver);
+            if (fuser.equalsIgnoreCase("close")) {
+                break;
+            }
+            if (fuser.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+
+        out.close();
+        in.close();
+        inu.close();
+        fromserver.close();
+  }
 }
